749a3ae906f36f2652723f5bb4ea11dc7168dc9e
Stanislav Kulikov <satsakul@yandex-team.ru>
2020-03-05 00:25:07 +0300

    app_components: create service

---
+++ service/src/main/java/com/mds2020/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.mds2020
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private val EXTRA_DATA = "extra_data"
+    private val EXTRA_COUNT = 100
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val colorButton = findViewById<Button>(R.id.color_button)
+        colorButton.setOnClickListener({
+            colorButton.setBackgroundColor(Random.nextInt(Int.MIN_VALUE, Int.MAX_VALUE));
+        })
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener({
+            for (i in 1..100) {
+                Thread.sleep(1000)
+                Log.d("MDS", "i = $i")
+            }
+        })
+    }
+
+    /*val intent = Intent()
+    //val intent = Intent(it.context, MyIntentService::class.java)
+    intent.putExtra(EXTRA_DATA, EXTRA_COUNT);
+    startService(intent);*/
+}
