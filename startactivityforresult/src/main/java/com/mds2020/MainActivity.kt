0e175c79bd8919c1f4461ce2eee5bb38ab86a5e4
Stanislav Kulikov <satsakul@yandex-team.ru>
2020-03-04 21:24:04 +0300

    app_components: create startActivityForResult

---
+++ startactivityforresult/src/main/java/com/mds2020/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.mds2020
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_CODE: Int = 1000
+    private val EXTRA_DATA: String = "extra_data";
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button);
+        button.setOnClickListener({
+            val intent = Intent();
+            intent.setClass(it.context, SecondActivity::class.java).apply {
+                putExtra("data", "value")
+            }
+            startActivityForResult(intent, REQUEST_CODE)
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                val value = data?.getStringExtra(EXTRA_DATA)
+                Toast.makeText(this, value, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ startactivityforresult/src/main/java/com/mds2020/SecondActivity.kt
@@ -0,0 +1,29 @@
+package com.mds2020
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class SecondActivity : AppCompatActivity() {
+
+    private val EXTRA_DATA: String = "extra_data";
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val data = intent.getStringExtra("data")
+        Toast.makeText(this, data, Toast.LENGTH_SHORT).show()
+
+        //adb shell am start com.mds2020/.SecondActivity
+    }
+
+    fun clickButton(view: View) {
+        val intent = Intent().putExtra(EXTRA_DATA, this.localClassName)
+        setResult(Activity.RESULT_OK, intent)
+        finish()
+    }
+}
