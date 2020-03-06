4b5a4c1bb3930d816880a4aa26e017f328aac9ab
Stanislav Kulikov <satsakul@yandex-team.ru>
2020-03-06 13:47:00 +0300

    master: migrate to androidx

--- fragment_navigation/src/main/java/com/example/shad/fragmentnavigation/MainActivity.kt
+++ fragment_navigation/src/main/java/com/example/shad/fragmentnavigation/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.shad.fragmentnavigation
 
 import android.os.Bundle
-
-
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
